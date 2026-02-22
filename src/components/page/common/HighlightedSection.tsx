255697a9f2de40232a09af9cfebae8667c0a1af7
Aman Palariya <55750085+amanpalariya@users.noreply.github.com>
2026-02-23 01:12:34 +0530

    chore: remove custom button wrapper

--- src/components/page/common/HighlightedSection.tsx
+++ src/components/page/common/HighlightedSection.tsx
@@ -1,7 +1,6 @@
-import { VStack, HStack, Icon, Link } from "@chakra-ui/react";
+import { Button, VStack, HStack, Icon, Link } from "@chakra-ui/react";
 import { InnerBgCard } from "../../core/Cards";
 import { SectionText } from "../../core/Texts";
-import { Button } from "@components/ui/button";
 import { useColorModeValue } from "@components/ui/color-mode";
 import React, { JSX } from "react";
 import NextLink from "next/link";
