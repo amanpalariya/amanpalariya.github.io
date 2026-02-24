017b7c3f9134ce50276754ea86bf3082394311a3
Aman Palariya <55750085+amanpalariya@users.noreply.github.com>
2026-02-24 22:05:15 +0530

    Upgrade Next.js to 16 (#23)
    * Upgrade to Next.js 16
    * Fix white flash on load and 404 page
    * Upgrade closed components

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,23 +3,29 @@
 import { Text, VStack } from "@chakra-ui/react";
 import { Center } from "@chakra-ui/react";
 import { Heading1, LinkText, SubtitleText } from "@components/core/Texts";
+import WithBackground from "@components/page/wrapper/WithBackground";
 import { PersonalData } from "data";
+import { Providers } from "./providers";
 
 export default function Error() {
   return (
-    <Center h={"100vh"}>
-      <VStack gap={8}>
-        <Heading1 centerAlign>Dear explorer, are you lost?</Heading1>
-        <Text>
-          <LinkText href="/">Go back home</LinkText>
-        </Text>
-        <SubtitleText>
-          Or contact the developer on{" "}
-          <LinkText href={PersonalData.linkedIn.url} isExternal>
-            LinkedIn
-          </LinkText>
-        </SubtitleText>
-      </VStack>
-    </Center>
+    <Providers>
+      <WithBackground>
+        <Center h={"100vh"}>
+          <VStack gap={8}>
+            <Heading1 centerAlign>Dear explorer, are you lost?</Heading1>
+            <Text>
+              <LinkText href="/">Go back home</LinkText>
+            </Text>
+            <SubtitleText>
+              Or contact the developer on{" "}
+              <LinkText href={PersonalData.linkedIn.url} isExternal>
+                LinkedIn
+              </LinkText>
+            </SubtitleText>
+          </VStack>
+        </Center>
+      </WithBackground>
+    </Providers>
   );
 }
