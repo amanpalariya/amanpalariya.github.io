b66ad9e9db273542eb1136e1c458b85f20238b0d
Aman Palariya <55750085+amanpalariya@users.noreply.github.com>
2026-03-17 17:17:03 +0530

    Update certificate URLs

--- src/data/cv/index.ts
+++ src/data/cv/index.ts
@@ -513,34 +513,38 @@ const CvData: CvData = {
           issuer: "Oracle",
           date: "2024-03",
           tags: ["AI"],
-          credentialUrl: "https://education.oracle.com/learn/ai-foundations/pP_AIFOUND",
+          credentialUrl:
+            "https://catalog-education.oracle.com/pls/certview/sharebadge?id=56429E42AFB4F1638B8AE52D1E2C8A9E9EA4927B04B09581E9EBC77750882328",
         },
         {
           title: "Oracle Certified Generative AI Professional",
           issuer: "Oracle",
           date: "2024-04",
-          tags: ["GenAI"],
-          credentialUrl: "https://education.oracle.com/learn/generative-ai/pP_GENAI",
+          tags: ["GenAI", "AI"],
+          credentialUrl:
+            "https://catalog-education.oracle.com/pls/certview/sharebadge?id=D12B6739007829DDBC7A67E24E241911E1893DD61ECDFBBAE4EF8A2551A5DA0C",
         },
         {
           title: "Oracle Certified OCI Foundations Associate",
           issuer: "Oracle",
           date: "2023-11",
           tags: ["OCI"],
-          credentialUrl: "https://education.oracle.com/learn/oci-foundations/pP_OCIFOUND",
+          credentialUrl:
+            "https://catalog-education.oracle.com/ords/certview/sharebadge?id=0068B0BCB206AD4CFFD64C75ACFCD947AD4ABAA366F2832E053E8509A614BCC6",
         },
         {
           title: "Oracle Certified Application Integration Professional",
           issuer: "Oracle",
           date: "2023-12",
           tags: ["Integration"],
-          credentialUrl: "https://education.oracle.com/learn/application-integration/pP_APPINT",
+          credentialUrl:
+            "https://catalog-education.oracle.com/ords/certview/sharebadge?id=9D2F0B9D632BFA694BF84A473233043D558B1AD966AA2DA4042FB7434B033E2F",
         },
         {
           title: "Kubernetes",
           issuer: "LinkedIn",
           date: "2024-01",
-          tags: ["Cloud Native"],
+          tags: ["Cloud"],
         },
       ],
     },
